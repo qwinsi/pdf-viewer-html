06fdd8cefe1b69c9ea2e2c8668bf9bf666711e2b
qwinsi <70425035+qwinsi@users.noreply.github.com>
2025-01-14 10:01:38 +0800

    open_file()

--- src/script.js
+++ src/script.js
@@ -1,3 +1,7 @@
+import { get as idbKeyvalGet, set as idbKeyvalSet } from "idb-keyval";
+
+const idbKeyval = { get: idbKeyvalGet, set: idbKeyvalSet };
+
 async function set_custom_background_color() {
     const preferences = window.PDFViewerApplication.preferences;
     let need_refresh = false;
@@ -21,6 +25,54 @@ function bind_keyboard_shortcuts() {
             window.PDFViewerApplication.pdfSidebar.toggle();
         }
     });
+
+    // Ctrl+O to open file
+    document.addEventListener('keydown', function (e) {
+        if (e.ctrlKey && e.key === 'o') {
+            // Prevent default Ctrl+O behavior of viewer.mjs and browser
+            e.stopPropagation();
+            e.preventDefault();
+
+            void open_pdf();
+        }
+    });
+}
+
+
+async function open_pdf() {
+    console.log('open_pdf');
+    const app = window.PDFViewerApplication;
+    const [fileHandle] = await window.showOpenFilePicker();
+
+    void idbKeyval.set('last_file_handle', fileHandle);
+
+    const file = await fileHandle.getFile();
+    const url = URL.createObjectURL(file);
+    app.open({
+        url: url,
+        originalUrl: file.name,
+    });
+}
+
+async function restore_last_pdf() {
+    const app = window.PDFViewerApplication;
+    const fileHandle = await idbKeyval.get('last_file_handle');
+    if (fileHandle) {
+        if(await fileHandle.queryPermission({ mode: 'read' }) !== 'granted') {
+            const res = await fileHandle.requestPermission({ mode: 'read' });
+            if(res !== 'granted') {
+                console.info('User denied permission to read file');
+                return;
+            }
+        }
+
+        const file = await fileHandle.getFile();
+        const url = URL.createObjectURL(file);
+        app.open({
+            url: url,
+            originalUrl: file.name,
+        });
+    }
 }
 
 
