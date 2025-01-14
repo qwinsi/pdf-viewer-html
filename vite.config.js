a8f779380e359d2cc9fe51fc2e327e89eaa1e399
qwinsi <70425035+qwinsi@users.noreply.github.com>
2025-01-14 17:59:54 +0800

    fix vite build error

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,28 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pdf-viewer-html',
+  base: '/pdf-viewer-html/',
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    // A disgusting behavior of vite. May be fixed in the future release.
+    // https://github.com/vitejs/vite/issues/15354
+    rollupOptions: {
+      external: ['/pdf-viewer-html/viewer.mjs'],
+      input: {
+        main: fileURLToPath(new URL('./index.html', import.meta.url)),
+      },
+      output: {
+        entryFileNames: 'main.js',
+        assetFileNames: 'main.css',
+        format: 'es',
+      },
     }
   }
 })
